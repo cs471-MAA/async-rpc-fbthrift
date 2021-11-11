dd41e728ec8b454ab87f535be72f593dc51ef387
Adrien Remillieux <adrien.remillieux@epfl.ch>
2021-11-11 11:21:18 +0100

    more some functions to shared/utils.h

--- src/MockDatabase/MockDatabase.cpp
+++ src/MockDatabase/MockDatabase.cpp
@@ -5,8 +5,8 @@
 #include <folly/executors/ThreadedExecutor.h>
 #include <folly/synchronization/Baton.h>
 #include <thrift/lib/cpp2/async/RocketClientChannel.h>
-#include <string>
 #include "MockDatabaseHandler.h"
+#include "Utils.h"
 
 using apache::thrift::ThriftServer;
 using apache::thrift::ThriftServerAsyncProcessorFactory;
@@ -19,22 +19,13 @@ using mock_message_board::MockDatabaseHandler;
 // using mock_message_board::MockDatabaseAsyncClient;
 using namespace std;
 
-std::unique_ptr<ThriftServer> newServer(folly::SocketAddress const &addr) {
-    auto handler = std::make_shared<MockDatabaseHandler>();
-    auto proc_factory = std::make_shared<ThriftServerAsyncProcessorFactory<MockDatabaseHandler>>(handler);
-    auto server = std::make_unique<ThriftServer>();
-    server->setAddress(addr);
-    server->setProcessorFactory(proc_factory);
-    return server;
-}
-
 int main(int argc, char *argv[]) {
     LOG(INFO) << "Starting test ...";
     FLAGS_logtostderr = 1;
     folly::init(&argc, &argv);
 
     folly::SocketAddress addr("mock-database", 10001, true);
-    auto server = newServer(addr);
+    auto server = newServer(addr, std::make_shared<MockDatabaseHandler>());
     LOG(INFO) << "server: starts";
     server->serve();
 }

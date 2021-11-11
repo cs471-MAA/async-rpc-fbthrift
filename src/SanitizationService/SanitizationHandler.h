445f2106b131f34b1a76b6c149fe06afcd965d9a
Saheru <adriansagerlaganga@gmail.com>
2021-11-11 12:24:45 +0100

    added basic regex sanitization

--- src/SanitizationService/SanitizationHandler.h
+++ src/SanitizationService/SanitizationHandler.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <glog/logging.h>
+#include <regex>
 #include <if/gen-cpp2/SanitizationService.h>
 
 namespace mock_message_board {
@@ -8,7 +9,14 @@ namespace mock_message_board {
     class SanitizationHandler : virtual public SanitizationServiceSvIf {
         public:
             bool sanitize_message(std::unique_ptr<::std::string> client_id, std::unique_ptr<::std::string> message) override {
-                LOG(INFO) << "server: received call: client_id=" << *client_id << " | message=" << *message;
+                std::regex match_expr("^.*[\\/*$^].*$");
+                if (std::regex_match(*message, match_expr)) {
+                    LOG(INFO) << "server: message is invalid";
+                    return false;
+                }
+
+                // TODO: bool store_message(1: string client_id, 2: string message)
+
                 return true;
             }
     };
